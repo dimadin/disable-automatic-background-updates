701ad6f09020de99fee79b0248371fab80d29892
Milan Dinić <milan@srpski.biz>
2018-05-24 20:07:23 +0200

    Remove action links from plugins page.

--- disable-automatic-background-updates.php
+++ disable-automatic-background-updates.php
@@ -47,34 +47,3 @@ add_filter( 'allow_minor_auto_core_updates', '__return_false' );
 
 // Disable core automatic background updates for major versions
 add_filter( 'allow_major_auto_core_updates', '__return_false' );
-
-/**
- * Add action links to plugins page.
- *
- * @since 1.1
- *
- * @param array  $links       Existing plugin's action links.
- * @param string $plugin_file Path to the plugin file.
- * @return array $links New plugin's action links.
- */
-function disable_automatic_background_updates_action_links( $links, $plugin_file ) {
-	// Set basename
-	$basename = plugin_basename( __FILE__ );
-
-	// Check if it is for this plugin
-	if ( $basename != $plugin_file ) {
-		return $links;
-	}
-
-	// Load translations
-	load_plugin_textdomain( 'disable-automatic-background-updates', false, dirname( $basename ) . '/languages' );
-
-	// Add new links
-	$links['donate']   = '<a href="http://blog.milandinic.com/donate/">' . __( 'Donate', 'disable-automatic-background-updates' ) . '</a>';
-	$links['wpdev']    = '<a href="http://blog.milandinic.com/wordpress/custom-development/">' . __( 'WordPress Developer', 'disable-automatic-background-updates' ) . '</a>';
-	$links['premiums'] = '<strong><a href="https://shop.milandinic.com/">' . __( 'Premium WordPress Plugins', 'disable-automatic-background-updates' ) . '</a></strong>';
-
-	return $links;
-}
-add_filter( 'plugin_action_links',               'disable_automatic_background_updates_action_links', 10, 2 );
-add_filter( 'network_admin_plugin_action_links', 'disable_automatic_background_updates_action_links', 10, 2 );
